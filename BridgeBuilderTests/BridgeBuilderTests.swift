b24ca11ef725b448ebee315f74576cb4ed7ac26f
Ryan Bergamini <rb265@duke.edu>
2017-06-09 09:47:46 -0400

    Initial Commit

---
+++ BridgeBuilderTests/BridgeBuilderTests.swift
@@ -0,0 +1,36 @@
+//
+//  BridgeBuilderTests.swift
+//  BridgeBuilderTests
+//
+//  Created by Ryan Bergamini on 6/9/17.
+//  Copyright © 2017 Duke Innovation CoLab. All rights reserved.
+//
+
+import XCTest
+@testable import BridgeBuilder
+
+class BridgeBuilderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
